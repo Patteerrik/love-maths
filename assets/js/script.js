2bad02ffd88febdcc1d95f1f5ed9e20af2cc132f
Patteerrik . <patrik.akerbergz@gmail.com>
2024-03-15 11:05:23 +0000

    Add code for subtraction game

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,7 +31,9 @@ function runGame(gameType) {
  if (gameType === "addition") {
     displayAdditionQuestion(num1, num2);
   } else if (gameType === "multiply") {
-    displayMultiplyQuestion(num1, num2);   
+    displayMultiplyQuestion(num1, num2); 
+  } else if (gameType === "subtract") {
+    displaySubtractQuestion(num1, num2);  
   } else {
     alert(`Unknown game type: ${gameType}`);
     throw `Unknown game type: ${gameType}. Aborting!`;
@@ -71,6 +73,8 @@ function calculateCorrectAnswer() {
         return [operand1 + operand2, "addition"];
     } else if (operator === "x") {
         return [operand1 * operand2, "multiply"];
+    } else if (operator === "-") {
+        return [operand1 - operand2, "subtract"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -106,7 +110,13 @@ function displayAdditionQuestion(operand1, operand2) {
     
 }
 
-function displaySubtractQuestion() {
+function displaySubtractQuestion(operand1, operand2) {
+    /**
+ * If operand1 is bigger than operator 2 ? show operand1 if false show operand 2
+ */
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "-";
 
 }
 
