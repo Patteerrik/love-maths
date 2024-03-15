d6b74cf85072c4b8a69d38a27c01bcf252a7fa1f
Patteerrik . <patrik.akerbergz@gmail.com>
2024-03-15 11:25:46 +0000

    Add favicon and final tidy ups for the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,8 +12,15 @@ document.addEventListener("DOMContentLoaded", function() {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
             }
-        });
+         });
     }
+
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    });
+
     runGame("addition");
 });
 
@@ -23,6 +30,11 @@ document.addEventListener("DOMContentLoaded", function() {
  */
 
 function runGame(gameType) {
+
+/* Makes number in answer box disappear after submitting and gets the cursor in place
+ */
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
 
  // Creates two random numbers between 1 and 25
  let num1 = Math.floor(Math.random() * 25)+ 1;
